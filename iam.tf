a8cb97abf517441e727bc05a2d483b8cc664115c
naoya-dev-code <0jx4d61y3871t0a@gmail.com>
2026-09-22 22:47:29 +0900

    Add AWS OIDC authentication for GitHub Actions

---
+++ iam.tf
@@ -0,0 +1,62 @@
+data "aws_caller_identity" "current" {}
+
+resource "aws_iam_role" "github_actions" {
+  name = "GitHubActions-Terraform-Deploy"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+
+    Statement = [
+      {
+        Effect = "Allow"
+
+        Principal = {
+          Federated = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:oidc-provider/token.actions.githubusercontent.com"
+        }
+
+        Action = "sts:AssumeRoleWithWebIdentity"
+
+        Condition = {
+          StringEquals = {
+            "token.actions.githubusercontent.com:aud" = "sts.amazonaws.com"
+          }
+
+          StringLike = {
+            "token.actions.githubusercontent.com:sub" = "repo:naoya-dev-code/terraform-github-actions:*"
+          }
+        }
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy" "github_actions_s3" {
+  name = "TerraformS3Access"
+  role = aws_iam_role.github_actions.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+
+    Statement = [
+      {
+        Effect = "Allow"
+
+        Action = [
+          "s3:CreateBucket",
+          "s3:DeleteBucket",
+          "s3:GetBucketTagging",
+          "s3:PutBucketTagging",
+          "s3:GetBucketPolicy",
+          "s3:GetBucketAcl",
+          "s3:GetBucketCors",
+          "s3:GetBucketWebsite",
+          "s3:GetBucketVersioning"
+        ]
+
+        Resource = [
+          "arn:aws:s3:::naoya-terraform-github-actions-demo"
+        ]
+      }
+    ]
+  })
+}
