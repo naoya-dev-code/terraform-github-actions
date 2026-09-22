2f94a41e615fe8d98d6d750ece9f250eb1ffe9d0
naoya-dev-code <0jx4d61y3871t0a@gmail.com>
2026-09-22 23:55:23 +0900

    Add IAM role read permissions for Terraform CI

--- iam.tf
+++ iam.tf
@@ -88,7 +88,8 @@ resource "aws_iam_role_policy" "github_actions_s3" {
         Action = [
           "iam:GetRole",
           "iam:GetRolePolicy",
-          "iam:ListRolePolicies"
+          "iam:ListRolePolicies",
+          "iam:ListAttachedRolePolicies"
         ]
 
         Resource = [
